63099fb0b10fa514e4ce9555d64a0f3f6497d88f
RohitAntil <rohit.ntil@gmail.com>
2015-10-16 00:48:14 +0530

    Initial commit

---
+++ app/src/main/java/com/example/acer/mymusic/Song.java
@@ -0,0 +1,18 @@
+package com.example.acer.mymusic;
+
+/**
+ * Created by Acer on 10/15/2015.
+ */
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
