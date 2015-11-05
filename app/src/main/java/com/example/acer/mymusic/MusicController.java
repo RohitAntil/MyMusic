152e57a946a686dafed096b9b1a39af83235248d
RohitAntil <rohit.ntil@gmail.com>
2015-11-05 21:11:46 +0530

    UI interface done

---
+++ app/src/main/java/com/example/acer/mymusic/MusicController.java
@@ -0,0 +1,16 @@
+package com.example.acer.mymusic;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Acer on 10/21/2015.
+ */
+public class MusicController extends MediaController{
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
